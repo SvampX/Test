b91da16316d6b52f65af4ffab8fa0efd1dd7b28f
Mikle Savchenko <svampx@gmail.com>
2020-02-12 01:54:55 +0200

    First master update from second repo with 3 branches

--- ClassMofucker.java
+++ ClassMofucker.java
@@ -1,5 +1,9 @@
+import java.lang.Math;
+
 public class ClassMofucker {
     public static void main(String[] args) {
         System.out.println("Buy"); //LOL
+    	System.out.println("Here comes Math!");
+	System.out.println(Math.PI);
     }
 }
