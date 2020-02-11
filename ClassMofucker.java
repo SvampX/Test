e7e81b3ae0aab968e6b560fa14f74f10a23858c1
Mikle Savchenko <svampx@gmail.com>
2020-02-12 00:45:11 +0200

    first commit of Secont repository

--- ClassMofucker.java
+++ ClassMofucker.java
@@ -1,5 +1,9 @@
 public class ClassMofucker {
     public static void main(String[] args) {
         System.out.println("Buy"); //LOL
+    	XPetu xPetu = new XPetu("Petu");
+	xPetu.setRank("Challenger");
+	xPetu.setLP(566);
+	xPetu.represent();
     }
 }
---
+++ XPetu.java
@@ -0,0 +1,38 @@
+class XPetu {
+	String name;
+	String rank;
+	int lP;
+	
+	XPetu(String name) {
+		this.name = name;
+	}
+	
+	void setName(String name) {
+		this.name = name;
+	}
+
+	String getName() {
+		return this.name;
+	}
+
+	void SetRank(String rank) {
+		this.rank = rank;
+	}
+
+	String getRank() {
+		return this.rank;
+	}
+
+	void setLP(int lP) {
+		this.lP = lP
+	}
+
+	int getLP() {
+		return this.lP;
+	}
+
+	void represent() {
+		System.out.println("I am " + getName() + " and I am " + getRank() + " my LP is " + getLP());
+	}
+
+}
