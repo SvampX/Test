e632e118279fad87837fcd6595bcf0fcee3a8d91
Mikle Savchenko <svampx@gmail.com>
2020-02-12 00:14:25 +0200

    first Repository first commit

--- ClassMofucker.java
+++ ClassMofucker.java
@@ -1,5 +1,6 @@
 public class ClassMofucker {
     public static void main(String[] args) {
         System.out.println("Buy"); //LOL
+    	System.out.println("Ok there, the first is ok"); //Nope (:
     }
 }
